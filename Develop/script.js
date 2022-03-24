b0e341d04eb37f5a1b5c6a947ebab64c7e8f8fab
samlarson94 <samlarson94@gmail.com>
2022-03-23 20:42:01 -0500

    Built out user input variables and password character array's.  Started building out user prompts and input validation checks.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,39 @@
+alert("Hello, let's get you a new password.");
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Save User Input
+ var passwordLength = prompt("How many characters would you like?");
+  console.log(passwordLength);
+//ADD STEP TO MAKE SURE USER INPUTS NUMBER - IF/ELSE STATEMENT?
+      if (typeof passwordLength === 'number') {
+        alert("Nice Choice!");
+      }else{
+        alert("please refresh page and enter a number");
+      };
+
+ var specialCharacters = confirm("Would you like to use special characters?");
+  console.log(specialCharacters);
+ var lowercaseCharacters = confirm("Would you like to use lower case letters?");
+  console.log(lowercaseCharacters);
+ var uppercaseCharacters = confirm("Would you like to use upper case letters?");
+  console.log(uppercaseCharacters);
+ var symbols = confirm("Would you like to use symbols?");
+  console.log(symbols);
+ 
+ //Store characters to feed the password
+ var symbolsSource = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  console.log(symbolsSource);
+ var uppercaseSource = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+ var lowercaseSource = [];
+      toLower = function(x){
+        return x.toLowerCase();
+      };
+      lowercaseSource = uppercaseSource.map(toLower);
+        console.log(lowercaseSource);
+ var specialCharactersSource = [" ", "!", "", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "", "]", "^", "_", "`", "{", "|", "}", "~"]
+  console.log(specialCharactersSource); //Why Are " and \ messing with string?
 
 // Write password to the #password input
 function writePassword() {
@@ -7,11 +41,10 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 function generatePassword() {
-  return "password here";
+  return "Hello";
 };
 
 // Add event listener to generate button
@@ -19,7 +52,8 @@ generateBtn.addEventListener("click", writePassword);
 
 // VERIFY THAT generatePassword function is working above //
 
-// 1. Create variables to save user input from prompts
+// 1. Create variables to save user input from prompt
+
   //passwordLength - string, will need to turn it into a number (look up online - built in method)
   //specialCharacters - boolean (true or false)
   //numericalCharacters - boolean
@@ -55,4 +89,7 @@ generateBtn.addEventListener("click", writePassword);
 
 
 // Add style to make the app pop in a future portfolio //
+
+
+
 
