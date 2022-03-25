f703137f6074210cf20e4e3efa8e86301b7484dc
samlarson94 <samlarson94@gmail.com>
2022-03-24 19:29:27 -0500

    Added notes for future reference, adjusted some styling.

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,11 @@
-//This to trigger off of pushing the generate password button.
 
-//Store characters to feed the password
+// Add variable to kick off password generation - select from id in HTML.
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generate.addEventListener("click", writePassword);
+
+//Store characters to feed the password - Symbols, Uppercase, Lowercase, and Special Characters
 var symbolsSource = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(symbolsSource);
 var uppercaseSource = [
@@ -72,90 +77,18 @@ var specialCharactersSource = [
   "}",
   "~",
 ];
-console.log(specialCharactersSource); //Why Are " and \ messing with string?
+console.log(specialCharactersSource); //Note to self - Why Are " and \ characters messing with string?
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var symbolsSource = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var uppercaseSource = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-var lowercaseSource = [];
-toLower = function (x) {
-  return x.toLowerCase();
-};
-lowercaseSource = uppercaseSource.map(toLower);
-var specialCharactersSource = [
-  " ",
-  "!",
-  "",
-  "#",
-  "$",
-  "%",
-  "&",
-  "'",
-  "(",
-  ")",
-  "*",
-  "+",
-  ",",
-  "-",
-  ".",
-  "/",
-  ":",
-  ";",
-  "<",
-  "=",
-  ">",
-  "?",
-  "@",
-  "[",
-  "",
-  "]",
-  "^",
-  "_",
-  "`",
-  "{",
-  "|",
-  "}",
-  "~",
-];
-console.log(specialCharactersSource); //Why Are " and \ messing with string?
-
+// Create empty array to store randomized characters
 var randomArray = [];
 
-// Save User Input
-//ADD STEP TO MAKE SURE USER INPUTS NUMBER - IF/ELSE STATEMENT?
+// Add prompts and corresponding variables to store user input
 var passwordLength
 
 function generatePassword() {
   passwordLength = parseInt(prompt("How many characters would you like?"));
-  console.log(passwordLength);
+  
+  //ADD STEP TO MAKE SURE USER INPUTS A NUMBER - How to make this refresh after more than one error?
   if (passwordLength === "") {
     prompt("Error: Password Length Cannot be blank");
   } else if (typeof passwordLength !== "number") {
@@ -183,105 +116,74 @@ function generatePassword() {
   var symbols = confirm("Would you like to use symbols?");
   console.log(symbols);
 
-  //Verify that user has selected at least one input-type
+  //Verify that user has selected at least one of the input types
   if (
     specialCharacters == true ||
     lowercaseCharacters == true ||
     uppercaseCharacters == true ||
     symbols == true
   ) {
-    //generate password - else/if statement to feed and randomize arrays
   } else {
     alert("Please refresh page and select at least one data type");
   }
+  //Generate password - if statements to verify and feed random array
+    if (uppercaseCharacters == true) {
+      randomArray = randomArray.concat(uppercaseSource);
+    }
+    if (lowercaseCharacters == true) {
+      randomArray = randomArray.concat(lowercaseSource);
+    }
+    if (specialCharacters == true) {
+      randomArray = randomArray.concat(specialCharactersSource);
+    }
+    if (symbols == true) {
+      randomArray = randomArray.concat(symbolsSource)
+    }
+    console.log(randomArray);
 
-  if (uppercaseCharacters == true) {
-    randomArray = randomArray.concat(uppercaseSource);
-  }
-  if (lowercaseCharacters == true) {
-    randomArray = randomArray.concat(lowercaseSource);
-  }
-  if (specialCharacters == true) {
-    randomArray = randomArray.concat(specialCharactersSource);
-  }
-  if (symbols == true) {
-    randomArray = randomArray.concat(symbolsSource)
-  }
-  //Add symbols and special characters
-  console.log("hello");
-  console.log(randomArray);
+//Randomize randomArray and feed it into new scrambledArray to pull the password from
 
-  //Randomize the new randomArray
-
-  var encryptedArray = randomArray.sort(function(a, b) {
+  var scrambledArray = randomArray.sort(function(a, b) {
     return 0.5 - Math.random()
   });
   console.log(randomArray);
-  console.log(encryptedArray);
+  console.log(scrambledArray);
 
-
+//Create new passwordArray that will store the correct amount of symbols/characters from the scrambledArray
 var passwordArray = [];
 console.log(typeof passwordLength);
+
+//Create for-loop that randomizes scrambledArray, stores in new variable, and then 
 for (let i = 0; i < passwordLength; i++) {
-  var randomCharacter = encryptedArray[Math.floor(Math.random()*encryptedArray.length)];
+  var randomCharacter = scrambledArray[Math.floor(Math.random()*scrambledArray.length)];
   console.log(randomCharacter);
   passwordArray.push(randomCharacter);
-  console.log()
+  console.log("Random Characters: " + randomCharacter)
 }
 
 return passwordArray.join("");
 }
 
-// Write password to the #password input
+// Write password to the #password id input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
-// function generatePassword() {
-//   return "Add generated password here";
-// };
 
-// Add event listener to generate button
-generate.addEventListener("click", writePassword);
 
 // VERIFY THAT generatePassword function is working above //
 
-// 1. Create variables to save user input from prompt
+// Additional Stying Practice //
 
-//passwordLength - string, will need to turn it into a number (look up online - built in method)
-//specialCharacters - boolean (true or false)
-//numericalCharacters - boolean
-//lowercaseCharacters - boolean
-//uppercaseCharacters - boolean
-//alert, confirm, and prompt functions used here
-//use alert function with an if statement (conditionals) to validate user input - Return "reload and enter reqd password" message if failed.
-//validate input to make sure user selects at least 1 character type
+document.getElementById("password").style.color = "green";
 
-// 2. Make variables (arrays) to store characters that will be fed to the password
-// symbols
-// uppercase
-// lowercase
-// special characters (Verify List in README file)
-
-// 3. Use indexRandomizer function to select characters from each array
-
-// 4. generatePassword function -- take user input, grab characters grom arrays, and generate a password
-
-// 5. Create an empty array that represents the final password.
-
-// 6. Join the character arrays to gether based on user input
-
-// 7. Randomize characters from the joined arrays
-
-// 8. Push at least 1 character from each selected array type into final password array
-
-// 9. Transform the password array into a string
-//console.log(elements.join(''));
-// expected output: "FireAirWater"
-
-// . Should be able to continually generate passwords -- while loop?
-
-// Add style to make the app pop in a future portfolio //
+var changeClass = document.querySelector('.card');
+  changeClass.setAttribute('style', 'background-color: #00FF41')
+  changeClass.style.border = '5px solid black';
+  changeClass.style.boxShadow = '15px 15px black';
+  changeClass.style.display = 'flex';
+  changeClass.style.flexDirection = "column";
+  changeClass.style.justifyContent = 'center';
+  changeClass.style.alignItems = 'center';
